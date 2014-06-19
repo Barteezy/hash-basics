cb79c9416002c642eef65f2066f0082cd2c578dc
Luke Bartel <bartelluke@gmail.com>
2014-06-19 14:13:53 -0600

    added favorite_food.rb

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,5 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+"Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"].join(", ")}"
