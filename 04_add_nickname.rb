3d5ddf156e398663838fd46eb6789e8141543214
Luke Bartel <bartelluke@gmail.com>
2014-06-19 14:05:48 -0600

    added address 05 solution

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,5 +1,3 @@
 require_relative "person"
 
 # Add a nickname to the BJORN_BORG hash and print it out. Don't edit the person.rb file!
-BJORN_BORG["nickname"] = "Big Dog"
-puts BJORN_BORG["nickname"]
